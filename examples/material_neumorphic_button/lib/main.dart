25176226bf42d3ba62a67c35803af0bd94946d6c
Jonathan <gsmlg.com@gmail.com>
2023-07-20 13:27:04 +0800

    temp: Fix example.

--- examples/material_neumorphic_button/lib/main.dart
+++ examples/material_neumorphic_button/lib/main.dart
@@ -54,38 +54,68 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    // final theme = Theme.of(context);
     return Scaffold(
-      body: NeumorphicBackground(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
+      body: SafeArea(
+        child: NeumorphicBackground(
+          child: Center(
+            child: Neumorphic(
+              child: SizedBox(
+                width: 480.0,
+                height: 320.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text(
+                      'You have pushed the button this many times:',
+                    ),
+                    const SizedBox(height: 20),
+                    SelectableText('$_counter',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineMedium!
+                            .copyWith(
+                              color: Theme.of(context).colorScheme.primary,
+                            )),
+                    const SizedBox(height: 20),
+                    NeumorphicButton(
+                      onPressed: _incrementCounter,
+                      tooltip: 'Increment',
+                      child: const Text('Click Me to +1'),
+                    ),
+                    const SizedBox(height: 10),
+                    Divider(
+                      // height: 1,
+                      // thickness: 5,
+                      indent: 20,
+                      endIndent: 20,
+                      color: Theme.of(context).colorScheme.onSecondaryContainer,
+                    ),
+                    const SizedBox(height: 10),
+                    NeumorphicButton(
+                      style: const NeumorphicStyle(
+                        depth: -2,
+                      ),
+                      onPressed: _decrementCounter,
+                      tooltip: 'Decrement',
+                      child: const Text('Click Me to -1'),
+                    ),
+                  ],
+                ),
               ),
-              const SizedBox(height: 20),
-              Text('$_counter',
-                  style: Theme.of(context).textTheme.headlineMedium!.copyWith(
-                        color: Theme.of(context).colorScheme.primary,
-                      )),
-              const SizedBox(height: 20),
-              NeumorphicButton(
-                onPressed: _incrementCounter,
-                tooltip: 'Increment',
-                child: const Text('Click Me to Plus One'),
-              ),
-            ],
+            ),
           ),
         ),
       ),
--- packages/material_neumorphic_background/bin/material_neumorphic_background.dart
+++
@@ -1,5 +0,0 @@
-import 'package:material_neumorphic_background/material_neumorphic_background.dart' as material_neumorphic_background;
-
-void main(List<String> arguments) {
-  print('Hello world: ${material_neumorphic_background.calculate()}!');
-}
