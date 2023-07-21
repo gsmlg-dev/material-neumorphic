68b85426c79a25e9cb5945362c5dbe529f664fd1
Jonathan <gsmlg.com@gmail.com>
2023-07-21 13:37:07 +0800

    temp: Add icon.

--- packages/material_neumorphic_icon/lib/material_neumorphic_icon.dart
+++ packages/material_neumorphic_icon/lib/material_neumorphic_icon.dart
@@ -1,3 +1,46 @@
-int calculate() {
-  return 6 * 7;
+import 'package:flutter/material.dart' hide AnimatedScale;
+import 'package:material_neumorphic_theme/material_neumorphic_theme.dart';
+import 'package:material_neumorphic_text/material_neumorphic_text.dart';
+
+class NeumorphicIcons {
+  NeumorphicIcons._();
+
+  static const _kFontFam = 'NeumorphicIcons';
+  static const _kFontPkg = "flutter_neumorphic";
+
+  static const IconData check =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
+
+@immutable
+class NeumorphicIcon extends StatelessWidget {
+  final IconData icon;
+  final NeumorphicStyle? style;
+  final Curve curve;
+  final double size;
+  final Duration duration;
+
+  NeumorphicIcon(
+    this.icon, {
+    Key? key,
+    this.duration = Neumorphic.DEFAULT_DURATION,
+    this.curve = Neumorphic.DEFAULT_CURVE,
+    this.style,
+    this.size = 20,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return NeumorphicText(
+      String.fromCharCode(icon.codePoint),
+      textStyle: NeumorphicTextStyle(
+        fontSize: size,
+        fontFamily: icon.fontFamily,
+        package: icon.fontPackage,
+      ),
+      duration: duration,
+      style: style,
+      curve: curve,
+    );
+  }
 }
